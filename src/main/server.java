84ff0496056245fca37581d5d5d05903c39f667b
Joel Seidel <jds6294@psu.edu>
2019-07-09 13:42:46 -0400

    Fix server start bug

--- src/main/server.java
+++ src/main/server.java
@@ -1,56 +1,72 @@
 package main;
-
 import com.sun.net.httpserver.HttpsConfigurator;
 import com.sun.net.httpserver.HttpsParameters;
 import com.sun.net.httpserver.HttpsServer;
-import main.handlers.AuthenticateUserHandler;
+import main.handlers.*;
+import main.data.DatabaseInteraction;
 
 import javax.net.ssl.*;
 import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.security.KeyStore;
-import java.security.NoSuchAlgorithmException;
 
 public class server {
-    public static void main(String args[]) {
+    public static void main(String args[]){
         try{
-            InetSocketAddress address = new InetSocketAddress(1680);
+            //Create socket address
+            InetSocketAddress address = new InetSocketAddress(6969);
+
+            //Initialize https server
             HttpsServer server = HttpsServer.create(address, 0);
             SSLContext sslContext = SSLContext.getInstance("TLS");
+
+            //Initialize the keystore
             char[] password = "password".toCharArray();
             KeyStore keyStore = KeyStore.getInstance("JKS");
-            InputStream keyStream = main.server.class.getClassLoader().getResourceAsStream("testkey.jks");
-            keyStore.load(keyStream, password);
+            InputStream inputStream = DatabaseInteraction.class.getClassLoader().getResourceAsStream("main/data/testkey.jks");
+            keyStore.load(inputStream, password);
+
+            //Create key manager factory
             KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
             keyManagerFactory.init(keyStore, password);
+
+            //Create the trust manager factory
             TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("SunX509");
             trustManagerFactory.init(keyStore);
+
+            //Create the HTTPS context and parameters
             sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
-            server.setHttpsConfigurator(new HttpsConfigurator(sslContext) {
-                public void configure(HttpsParameters params) {
-                    try {
+            server.setHttpsConfigurator(new HttpsConfigurator(sslContext){
+                public void configure(HttpsParameters params){
+                    try{
+                        //Init SSL context
                         SSLContext context = SSLContext.getDefault();
                         SSLEngine engine = context.createSSLEngine();
                         params.setNeedClientAuth(false);
                         params.setCipherSuites(engine.getEnabledCipherSuites());
                         params.setProtocols(engine.getEnabledProtocols());
-                        SSLParameters defaultSslParams = context.getDefaultSSLParameters();
-                        params.setSSLParameters(defaultSslParams);
-                    } catch (NoSuchAlgorithmException nsae) {
-                        nsae.printStackTrace();
+
+                        //Fetch default params
+                        SSLParameters defaultSSLParams = context.getDefaultSSLParameters();
+                        params.setSSLParameters(defaultSSLParams);
+                    } catch(Exception ex){
+                        ex.printStackTrace();
                     }
                 }
             });
             //Create server event handler context
-            server = createHandlerContext(server);
+            server = createHandlerContexts(server);
+            //Start the server instance and hope for the best
             server.setExecutor(null);
             server.start();
-            System.out.println("Server running and listening on " + address);
-        } catch (Exception ex) {
+            //Debug
+            System.out.println("Server Running and Listening On " + address);
+        } catch(Exception ex){
             ex.printStackTrace();
         }
     }
-    private static HttpsServer createHandlerContext(HttpsServer server){
+
+    private static HttpsServer createHandlerContexts(HttpsServer server){
         server.createContext("/authenticate_user", new AuthenticateUserHandler());
         return server;
     }
