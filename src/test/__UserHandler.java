a25473146b6a7636bcf1dabe579a30533f86d2b4
Joel Seidel <jds6294@psu.edu>
2019-07-15 11:20:19 -0400

    Edit user action test

--- src/test/__UserHandler.java
+++ src/test/__UserHandler.java
@@ -2,6 +2,7 @@ package test;
 
 import main.data.DatabaseInteraction;
 import main.handlers.UserHandler;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Test;
 
@@ -14,17 +15,7 @@ public class __UserHandler {
     private static final String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsb2NhbGhvc3Q6Njk2OSJ9.BtqugmuS7W-RcSHk_iiTwSk10DNgcmnUhKoe9JFTAqk";
     @Test
     public void __CreateUser(){
-        JSONObject createUserRequest = new JSONObject();
-        createUserRequest.put("action", "create");
-        createUserRequest.put("token", token);
-        createUserRequest.put("username", "realDonaldTrump");
-        createUserRequest.put("password", "thanksobama");
-        createUserRequest.put("firstname", "Donald");
-        createUserRequest.put("lastname", "Trump");
-        createUserRequest.put("organization", 69);
-        createUserRequest.put("access", 1);
-        UserHandler handler = new UserHandler();
-        handler.fulfillRequest(createUserRequest);
+        createTestUserProfile();
         DatabaseInteraction database = new DatabaseInteraction();
         ResultSet createUserResults = database.query(database.prepareStatement("SELECT * FROM Users WHERE username = 'realDonaldTrump'"));
         try {
@@ -42,5 +33,52 @@ public class __UserHandler {
         //Test has passed. Clean up database entries
         database.nonQuery(database.prepareStatement("DELETE FROM Users WHERE username = 'realDonaldTrump'"));
         database.closeConnection();
+    }
+
+    @Test
+    public void __EditUser(){
+        createTestUserProfile();
+        UserHandler handler = new UserHandler();
+        JSONObject editRequest = new JSONObject();
+        editRequest.put("action", "edit");
+        editRequest.put("token", token);
+        JSONArray editFields = new JSONArray();
+        editRequest.put("username", "realDonaldTrump");
+        JSONObject editFirstNameObj = new JSONObject();
+        editFirstNameObj.put("fieldname", "firstname");
+        editFirstNameObj.put("newval", "Vladimir");
+        editFields.put(editFirstNameObj);
+        JSONObject editLastNameObj = new JSONObject();
+        editLastNameObj.put("fieldname", "lastname");
+        editLastNameObj.put("newval", "Putin");
+        editFields.put(editLastNameObj);
+        editRequest.put("fields", editFields);
+        handler.fulfillRequest(editRequest);
+        DatabaseInteraction database = new DatabaseInteraction();
+        ResultSet editUserResults = database.query(database.prepareStatement("SELECT * FROM Users WHERE username = 'realDonaldTrump'"));
+        try{
+            editUserResults.next();
+            assertEquals("Vladimir", editUserResults.getString("firstname"));
+            assertEquals("Putin", editUserResults.getString("lastname"));
+        } catch(SQLException sqlEx) {
+            fail();
+        }
+        //Clean up database entries
+        database.nonQuery(database.prepareStatement("DELETE FROM Users WHERE username = 'realDonaldTrump'"));
+        database.closeConnection();
+    }
+
+    private void createTestUserProfile(){
+        JSONObject createUserRequest = new JSONObject();
+        createUserRequest.put("action", "create");
+        createUserRequest.put("token", token);
+        createUserRequest.put("username", "realDonaldTrump");
+        createUserRequest.put("password", "thanksobama");
+        createUserRequest.put("firstname", "Donald");
+        createUserRequest.put("lastname", "Trump");
+        createUserRequest.put("organization", 69);
+        createUserRequest.put("access", 1);
+        UserHandler handler = new UserHandler();
+        handler.fulfillRequest(createUserRequest);
     }
 }
